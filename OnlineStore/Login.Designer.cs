67b755f2270a4af2e5c2e79b5d0e469e35bac11f
Denis <bigdramashow730@gmail.com>
2024-10-26 21:24:15 +0300

    init

---
+++ OnlineStore/Login.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace OnlineStore
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            panel1 = new Panel();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            linkLabel1 = new LinkLabel();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            button2 = new Button();
+            RegisterBtn = new Button();
+            LoginBtn = new Button();
+            PasswordPanel = new Panel();
+            PasswordTextBox = new TextBox();
+            PasswordPictureBox = new PictureBox();
+            LoginPanel = new Panel();
+            LoginTextBox = new TextBox();
+            pictureBox2 = new PictureBox();
+            CloseBtn = new Button();
+            label5 = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            PasswordPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)PasswordPictureBox).BeginInit();
+            LoginPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(48, 128, 185);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(300, 530);
+            panel1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Century Gothic", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(135, 502);
+            label4.Name = "label4";
+            label4.Size = new Size(144, 16);
+            label4.TabIndex = 1;
+            label4.Text = "© 2024 All Rights Reserved";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(29, 272);
+            label3.Name = "label3";
+            label3.Size = new Size(242, 24);
+            label3.TabIndex = 1;
+            label3.Text = "Shop smart, live better.";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(128, 238);
+            label2.Name = "label2";
+            label2.Size = new Size(132, 24);
+            label2.TabIndex = 1;
+            label2.Text = "Store Name";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(31, 204);
+            label1.Name = "label1";
+            label1.Size = new Size(178, 24);
+            label1.TabIndex = 1;
+            label1.Text = "Welcome to the";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(95, 40);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(120, 120);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(linkLabel1);
+            panel2.Controls.Add(label8);
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(RegisterBtn);
+            panel2.Controls.Add(LoginBtn);
+            panel2.Controls.Add(PasswordPanel);
+            panel2.Controls.Add(LoginPanel);
+            panel2.Controls.Add(CloseBtn);
+            panel2.Controls.Add(label5);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(300, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(450, 530);
+            panel2.TabIndex = 1;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.LinkColor = Color.FromArgb(41, 128, 185);
+            linkLabel1.Location = new Point(200, 501);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(87, 15);
+            linkLabel1.TabIndex = 4;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "test@mail.com";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Century Gothic", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label8.ForeColor = Color.Silver;
+            label8.Location = new Point(39, 500);
+            label8.Name = "label8";
+            label8.Size = new Size(162, 16);
+            label8.TabIndex = 1;
+            label8.Text = "submit an email message to :";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Century Gothic", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label7.ForeColor = Color.Silver;
+            label7.Location = new Point(39, 484);
+            label7.Name = "label7";
+            label7.Size = new Size(280, 16);
+            label7.TabIndex = 1;
+            label7.Text = "To obratin acces this App or any quastions about it";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Century Gothic", 8F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label6.ForeColor = Color.Silver;
+            label6.Location = new Point(39, 468);
+            label6.Name = "label6";
+            label6.Size = new Size(56, 16);
+            label6.TabIndex = 1;
+            label6.Text = "Support :";
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.Control;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Century Gothic", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button2.ForeColor = Color.FromArgb(41, 128, 185);
+            button2.Location = new Point(294, 399);
+            button2.Name = "button2";
+            button2.Size = new Size(120, 25);
+            button2.TabIndex = 3;
+            button2.Text = "Forgot Password?";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // RegisterBtn
+            // 
+            RegisterBtn.BackColor = Color.FromArgb(41, 128, 185);
+            RegisterBtn.FlatStyle = FlatStyle.Flat;
+            RegisterBtn.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            RegisterBtn.ForeColor = Color.White;
+            RegisterBtn.Location = new Point(274, 310);
+            RegisterBtn.Name = "RegisterBtn";
+            RegisterBtn.Size = new Size(148, 35);
+            RegisterBtn.TabIndex = 3;
+            RegisterBtn.Text = "REGISTER";
+            RegisterBtn.UseVisualStyleBackColor = false;
+            // 
+            // LoginBtn
+            // 
+            LoginBtn.BackColor = Color.FromArgb(41, 128, 185);
+            LoginBtn.FlatStyle = FlatStyle.Flat;
+            LoginBtn.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            LoginBtn.ForeColor = Color.White;
+            LoginBtn.Location = new Point(15, 310);
+            LoginBtn.Name = "LoginBtn";
+            LoginBtn.Size = new Size(148, 35);
+            LoginBtn.TabIndex = 3;
+            LoginBtn.Text = "LOGIN";
+            LoginBtn.UseVisualStyleBackColor = false;
+            // 
+            // PasswordPanel
+            // 
+            PasswordPanel.BackColor = SystemColors.Control;
+            PasswordPanel.Controls.Add(PasswordTextBox);
+            PasswordPanel.Controls.Add(PasswordPictureBox);
+            PasswordPanel.Location = new Point(0, 235);
+            PasswordPanel.Name = "PasswordPanel";
+            PasswordPanel.Size = new Size(450, 45);
+            PasswordPanel.TabIndex = 2;
+            // 
+            // PasswordTextBox
+            // 
+            PasswordTextBox.BackColor = SystemColors.Control;
+            PasswordTextBox.BorderStyle = BorderStyle.None;
+            PasswordTextBox.Font = new Font("Century Gothic", 10F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            PasswordTextBox.ForeColor = Color.FromArgb(41, 128, 185);
+            PasswordTextBox.Location = new Point(55, 12);
+            PasswordTextBox.Name = "PasswordTextBox";
+            PasswordTextBox.Size = new Size(370, 17);
+            PasswordTextBox.TabIndex = 3;
+            PasswordTextBox.UseSystemPasswordChar = true;
+            PasswordTextBox.Click += PasswordTextBox_Click;
+            // 
+            // PasswordPictureBox
+            // 
+            PasswordPictureBox.Image = (Image)resources.GetObject("PasswordPictureBox.Image");
+            PasswordPictureBox.Location = new Point(15, 11);
+            PasswordPictureBox.Name = "PasswordPictureBox";
+            PasswordPictureBox.Size = new Size(24, 24);
+            PasswordPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            PasswordPictureBox.TabIndex = 0;
+            PasswordPictureBox.TabStop = false;
+            PasswordPictureBox.MouseDown += PasswordPictureBox_MouseDown;
+            PasswordPictureBox.MouseUp += PasswordPictureBox_MouseUp;
+            // 
+            // LoginPanel
+            // 
+            LoginPanel.BackColor = Color.White;
+            LoginPanel.Controls.Add(LoginTextBox);
+            LoginPanel.Controls.Add(pictureBox2);
+            LoginPanel.Location = new Point(0, 185);
+            LoginPanel.Name = "LoginPanel";
+            LoginPanel.Size = new Size(450, 45);
+            LoginPanel.TabIndex = 2;
+            // 
+            // LoginTextBox
+            // 
+            LoginTextBox.BorderStyle = BorderStyle.None;
+            LoginTextBox.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            LoginTextBox.ForeColor = Color.FromArgb(41, 128, 185);
+            LoginTextBox.Location = new Point(55, 12);
+            LoginTextBox.Name = "LoginTextBox";
+            LoginTextBox.Size = new Size(370, 20);
+            LoginTextBox.TabIndex = 3;
+            LoginTextBox.Click += LoginTextBox_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(15, 11);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(24, 24);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // CloseBtn
+            // 
+            CloseBtn.Cursor = Cursors.Hand;
+            CloseBtn.FlatAppearance.BorderSize = 0;
+            CloseBtn.FlatStyle = FlatStyle.Flat;
+            CloseBtn.Font = new Font("Verdana", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            CloseBtn.ForeColor = Color.FromArgb(41, 128, 185);
+            CloseBtn.Location = new Point(410, 0);
+            CloseBtn.Name = "CloseBtn";
+            CloseBtn.Size = new Size(40, 40);
+            CloseBtn.TabIndex = 0;
+            CloseBtn.Text = "X";
+            CloseBtn.UseVisualStyleBackColor = true;
+            CloseBtn.Click += CloseBtn_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label5.ForeColor = Color.FromArgb(41, 128, 185);
+            label5.Location = new Point(38, 135);
+            label5.Name = "label5";
+            label5.Size = new Size(236, 24);
+            label5.TabIndex = 1;
+            label5.Text = "Login to your account";
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(750, 530);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Name = "Login";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            PasswordPanel.ResumeLayout(false);
+            PasswordPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)PasswordPictureBox).EndInit();
+            LoginPanel.ResumeLayout(false);
+            LoginPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label3;
+        private Label label2;
+        private Label label4;
+        private Button CloseBtn;
+        private Label label5;
+        private Panel PasswordPanel;
+        private Panel LoginPanel;
+        private PictureBox pictureBox2;
+        private TextBox LoginTextBox;
+        private PictureBox PasswordPictureBox;
+        private TextBox PasswordTextBox;
+        private Button LoginBtn;
+        private Button button2;
+        private Label label6;
+        private Label label8;
+        private Label label7;
+        private LinkLabel linkLabel1;
+        private Button RegisterBtn;
+    }
+}
---
+++ OnlineStore/Login.cs
@@ -0,0 +1,45 @@
+using System.Windows.Forms;
+using System.Drawing;
+
+
+namespace OnlineStore
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void LoginTextBox_Click(object sender, EventArgs e)
+        {
+            LoginTextBox.BackColor = Color.White;
+            LoginPanel.BackColor = Color.White;
+            PasswordPanel.BackColor = SystemColors.Control;
+            PasswordTextBox.BackColor = SystemColors.Control;
+        }
+
+        private void PasswordTextBox_Click(object sender, EventArgs e)
+        {
+            PasswordTextBox.BackColor = Color.White;
+            PasswordPanel.BackColor = Color.White;
+            LoginTextBox.BackColor = SystemColors.Control;
+            LoginPanel.BackColor = SystemColors.Control;
+        }
+
+        private void PasswordPictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            PasswordTextBox.UseSystemPasswordChar = false;
+        }
+
+        private void PasswordPictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            PasswordTextBox.UseSystemPasswordChar = true;
+        }
+    }
+}
